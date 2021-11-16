1ba8e46303434822243c4d3b5b4f5b5198452ca7
Sanches Spoladore <sanches.lvs@gmail.com>
2021-11-15 22:38:56 -0300

    Aula 5

--- script.js
+++ script.js
@@ -32,7 +32,13 @@ const swiper = new Swiper('.swiper-container', {
     el: '.swiper-pagination'
   },
   mousewheel: true,
-  keyboard: true
+  keyboard: true,
+  breakpoints: {
+    767: {
+      slidesPerView: 2,
+      setWrapperSize: true
+    }
+  }
 })
 
 const scrollReveal = ScrollReveal({
@@ -47,7 +53,7 @@ scrollReveal.reveal(
   #about .image, #about .text,
   #services header, #services .card,
   #testimonials header, #testimonials .testimonials
-  #contact .text, #contact .links
+  #contact .text, #contact .links,
   footer .brand, footer .social
   `,
   { interval: 100 }
@@ -63,9 +69,32 @@ function backToTop() {
   }
 }
 
-window.addEventListener('scroll', () => {})
+function activateMenuAtCurrentSection() {
+  const sections = document.querySelectorAll('main section[id]')
+  const checkpoint = window.pageYOffset + (window.innerHeight / 8) * 4
 
-window.addEventListener('scroll', () => {
+  for (const section of sections) {
+    const sectionTop = section.offsetTop
+    const sectionHeight = section.offsetHeight
+    const sectionId = section.getAttribute('id')
+
+    const checkpointStart = checkpoint >= sectionTop
+    const checkpointEnd = checkpoint <= sectionTop + sectionHeight
+
+    if (checkpointStart && checkpointEnd) {
+      document
+        .querySelector('nav ul li a[href*=' + sectionId + ']')
+        .classList.add('active')
+    } else {
+      document
+        .querySelector('nav ul li a[href*=' + sectionId + ']')
+        .classList.remove('active')
+    }
+  }
+}
+
+window.addEventListener('scroll', function () {
   changeHeaderWhenScroll()
   backToTop()
+  activateMenuAtCurrentSection()
 })
